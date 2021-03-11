4da6668b6fdb1bc63757d216835713fde91b9863
orlando <75084509+dehoisted@users.noreply.github.com>
2021-03-10 20:11:33 -0500

    Create lag.lua

---
+++ RBLXLag_Switch/lag.lua
@@ -0,0 +1,19 @@
+-- lag.lua
+TextButton.MouseButton1Click:Connect(function()
+    if run then
+        run = false
+        args = 0.1
+        settings():GetService("NetworkSettings").IncomingReplicationLag = args
+        gcinfo()
+        collectgarbage("count")
+        wait(2)
+        print("Stopped Lag!")
+        print("Replication Lag = "..args)
+    else
+        args = 15
+        run = true
+        print("started");
+        print("rep lag = "..args)
+        settings():GetService("NetworkSettings").IncomingReplicationLag = args
+    end
+end)
