4c62dae0f7d6fb8953aef636a9dd68775a574ca3
orlando <75084509+dehoisted@users.noreply.github.com>
2021-03-14 19:03:33 -0400

    Update main.lua

--- RBLXLag_Switch/main.lua
+++ RBLXLag_Switch/main.lua
@@ -1,10 +1,22 @@
-local keybinds = 
-                  {["Rejoin"] = "Rejoins the game",
-                  ["Server Hop"] = "Teleports to random local server"}
-              
---Press 'P' to rejoin the game
-discord1 = "orlando#1337"
--- check.lua
+-- Full Script -- made by orlando
+
+-- def.lua -- early
+keybind1 = 'p'
+keybind2 = 'u'
+
+--[[
+Amount of Replicated Lag
+Increase it and more lag
+Vice Versa
+--]]
+
+args = 15
+game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Info";
+        Text = "Check your dev console for information (F9)";
+      })
+  
+-- check.lua: code for checking user before anything else
 if NA_LOADED and not _G.NA_DEBUG == true then
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "ERROR";
@@ -24,19 +36,13 @@ end
 
 local fake = function() return 'asd' end
 if not is_synapse_function(fake) then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "ERROR";
+        Text = "Synapse Only";
+      })
     error("Use Synapse u skid")
     return
 end
-
-
--- Main
-args = 15 -- (lower = less lag)
-print("-----     -----")
-print("Lag Switch 1.2")
-print("Made by "..discord1.." on discord")
-print("Press 'P' to rejoin the game")
-print("Current Replication Lag = "..args)
-print("-----     -----");
 
 -- gui.lua
 local ScreenGui = Instance.new("ScreenGui")
@@ -44,15 +50,15 @@ local Frame = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
 ScreenGui.Parent = game.CoreGui
 Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.new(1, 0.388235, 0.368627)
-Frame.BorderColor3 = Color3.new(0.67451, 0.211765, 0.152941)
+Frame.BackgroundColor3 = Color3.fromRGB(33, 158, 207)
+Frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
 Frame.Position = UDim2.new(0.293040276, 0, 0.491666675, 0)
 Frame.Size = UDim2.new(0.106227107, 0, 0.0833333284, 0)
 Frame.Active = true
 Frame.Draggable = true
 TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.new(1, 1, 1)
-TextButton.BackgroundTransparency = 0.80000001192093
+TextButton.BackgroundColor3 = Color3.fromRGB(33, 195, 207)
+TextButton.BackgroundTransparency = 0.80000001192095
 TextButton.Position = UDim2.new(0.103524067, 0, 0.200333327, 0)
 TextButton.Size = UDim2.new(0.793684483, 0, 0.601000011, 0)
 TextButton.Font = Enum.Font.SourceSansLight
@@ -62,8 +68,8 @@ TextButton.TextScaled = true
 TextButton.TextSize = 14
 TextButton.TextWrapped = true
 
+-- lag.lua
 TextButton.MouseButton1Click:Connect(function()
-    --main.lua
     if run then
         run = false
         args = 0.1
@@ -82,12 +88,35 @@ TextButton.MouseButton1Click:Connect(function()
     end
 end)
 
-
 -- keybinds.lua
+print("** Keybinds **")
+binds_num = 4
+keybinds =  
+{"Rejoin", "Rejoins the current lobby you are in",
+"Server Hop", "Joins a random local server"} 
+              
+for i = 1, binds_num do
+    print(keybinds[i])
+end
+
 game.Players.LocalPlayer:GetMouse().KeyDown:connect(function(key)
-    if (key=="p") then
+    if (key==keybind1) then
       game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
-      gcinfo()
-      collectgarbage("count")
     end
+end)
+
+game.Players.LocalPlayer:GetMouse().KeyDown:connect(function(key)
+	if (key==keybind2) then
+		local x = {}
+		for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+			if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+				x[#x + 1] = v.id
+			end
+		end
+		if #x > 0 then
+			game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+		else
+		return warn("Couldn't find a server")
+		end
+	end
 end)
